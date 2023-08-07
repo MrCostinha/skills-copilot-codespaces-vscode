8ffae3ecc617dfa8d39d349ee6fabea099da77f9
Matheus Costa <matheuscostamc135@gmail.com>
2023-08-06 23:39:10 -0300

    step 4

---
+++ comments.js
@@ -0,0 +1,7 @@
+// Create web server
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.writeHead(200);
+    res.end('Hello, World!');
+});
+server.listen(3000);
